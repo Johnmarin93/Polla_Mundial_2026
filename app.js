516cd3863ae4c255614c84e1247d686c439bcaa8
Johm Marin <Johnmarin93@users.noreply.github.com>
2026-05-05 22:39:08 -0500

    fix: enable cors

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
+
+// Middleware
 app.use(express.json());
+// Habilitar CORS
+app.use(cors());
 
 // Puerto de escucha
 const PORT = process.env.PORTSERVIDOR || 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
 // Rutas
-import userRoutes from "./routes/user.routes.js";
 app.use("/api/users", userRoutes);
+
+// Ruta base
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀");
 });
